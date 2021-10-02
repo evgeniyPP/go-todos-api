48df62e9434249c013df9f8d7f721c1ef6e0c9fd
Eugene Petryankin <epetryankin@devvela.com>
2021-10-02 16:49:10 +0300

    Add clean architecture layers & DI

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/evgeniyPP/go-todos-api"
 	"github.com/evgeniyPP/go-todos-api/pkg/handler"
+	"github.com/evgeniyPP/go-todos-api/pkg/repository"
+	"github.com/evgeniyPP/go-todos-api/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todos.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server. Error: %s", err.Error())
