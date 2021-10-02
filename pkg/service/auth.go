2c5ccf82f69848d030e3e65b632988b53f4c691a
Eugene Petryankin <epetryankin@devvela.com>
2021-10-02 20:04:53 +0300

    Add sign in

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -4,7 +4,9 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/evgeniyPP/go-todos-api"
 	"github.com/evgeniyPP/go-todos-api/pkg/repository"
 )
@@ -20,6 +22,26 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todos.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &struct {
+		jwt.StandardClaims
+		UserId int `json:"user_id"`
+	}{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(12 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(os.Getenv("JWT_SIGNING_KEY")))
 }
 
 func generatePasswordHash(password string) string {
