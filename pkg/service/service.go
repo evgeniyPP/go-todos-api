988c921ee4df39b446d020afdbd6209ba1577715
Eugene Petryankin <epetryankin@devvela.com>
2021-10-02 19:36:44 +0300

    Add sign up

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,13 @@
 package service
 
-import "github.com/evgeniyPP/go-todos-api/pkg/repository"
+import (
+	"github.com/evgeniyPP/go-todos-api"
+	"github.com/evgeniyPP/go-todos-api/pkg/repository"
+)
 
-type Authorization interface{}
+type Authorization interface {
+	CreateUser(user todos.User) (int, error)
+}
 
 type TodoList interface{}
 
@@ -15,5 +20,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
