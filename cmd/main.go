245898b4a8d6ea6d2088441f5942c93981e1984d
Eugene Petryankin <epetryankin@devvela.com>
2021-10-02 18:45:52 +0300

    Connect DB

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/evgeniyPP/go-todos-api"
 	"github.com/evgeniyPP/go-todos-api/pkg/handler"
 	"github.com/evgeniyPP/go-todos-api/pkg/repository"
 	"github.com/evgeniyPP/go-todos-api/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -15,7 +18,23 @@ func main() {
 		log.Fatalf("error initilizing configs. Error: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables. Error: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to init db. Error: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
