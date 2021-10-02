bf6ac282016b1d894064a1577f91728b0a43b865
Eugene Petryankin <epetryankin@devvela.com>
2021-10-02 17:05:56 +0300

    Add config

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/evgeniyPP/go-todos-api/pkg/handler"
 	"github.com/evgeniyPP/go-todos-api/pkg/repository"
 	"github.com/evgeniyPP/go-todos-api/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initilizing configs. Error: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todos.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server. Error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
